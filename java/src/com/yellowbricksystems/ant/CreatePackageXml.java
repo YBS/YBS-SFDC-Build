681aeb626d94a6406ce6df3f99c0bc65c71886b5
Jeff Bohanek <jeff@yellowbricksystems.com>
2020-05-17 14:12:54 -0400

    45.2a Fix build to support Java 11 without extra blank lines

--- java/src/com/yellowbricksystems/ant/CreatePackageXml.java
+++ java/src/com/yellowbricksystems/ant/CreatePackageXml.java
@@ -63,7 +63,7 @@ import com.sforce.soap.tooling.sobject.RecordType;
 
 public class CreatePackageXml extends SalesforceTask {
 
-	public static final String BUILD_VERSION = "45.2";
+	public static final String BUILD_VERSION = "45.2a";
 	
 	public static final String PERMISSION_SET_QUERY = "select Id,Name,NamespacePrefix from PermissionSet where ProfileId = null order by NamespacePrefix, Name";
 	
